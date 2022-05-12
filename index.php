824b277dc15bb8bda27c61cabfacab994240afca
Smekker11 <mekkerdavid@gmail.com>
2022-05-13 00:40:52 +0300

    finally in a presentable stage

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <body>
         <h1 style="color: yellow;" align="center">site mekke mobile<h1>
         <div id="div-menu">
+        <img src="figyelem.gif" id="figyelem"></img>
             <h5 id="ip">IP IS:
                 <?php
                 $ip = $_SERVER['REMOTE_ADDR'];
@@ -14,7 +15,16 @@
                 ?>
             </h5>
             <h5 id="plinks" style="color: orange;">PAGE LINKS:</h5>
-            <a></a>
+            <a href="https://github.com/Smekker11/" id="github">Github</a>
+            <a href="http://smekker.go.ro/old-home/hrusca-pusca/" id="hrusc">Hrusca Pusca</a>
+            <a href="https://smekker.go.ro:420/" id="idrac">iDRAC</a>
+            <a href="https://smekker.go.ro/log.txt" id="iplog">IP LOG :)</a>
+            <a href="http://smekker.go.ro:8123/" id="map">MAP 2kbps</a>
+            <img src="fold.gif" id="fold"></img>
+        </div>
+        <div id="div-bongus">
+            <img src="amgus.jpg" id="amgussy"></img>
+            <h1 style="color: lightblue;" align="center">sussy amgussy mekke site very AMOGS</h1>
         </div>
     </body>
 </html>
