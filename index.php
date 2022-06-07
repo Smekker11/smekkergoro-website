6612e40a85915f1756a13cc11c458bfd391c352a
Smekker11 <mekkerdavid@gmail.com>
2022-06-08 01:45:32 +0300

    kaki amangas holland

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
          ?>
         </marquee>
         <h1 style="color: yellow;" align="center">site mekke mobile<h1>
+        <div class="holland">
+         <img id="cataclysm" src="mencke-cataclysm.jpg"></img>
+         <h1 align="center" style="color: red; background-color: orange;">the fat of the fard</h1>
+         <p align="center" style="color: black; background-color: green;">the holland fatass menace has finally returned from cardboard wordpress</p>
+        </div>
         <div id="div-menu">
         <img src="figyelem.gif" id="figyelem"></img>
             <h5 id="ip">IP IS:
@@ -32,6 +37,13 @@
         <div id="div-bongus">
             <img src="amgus.jpg" id="amgussy"></img>
             <h1 style="color: lightblue;" align="center">sussy amgussy mekke site very AMOGS</h1>
+            <button id="sussy-b" type="button" onclick="susSound();"n>amangas</button>
+            <script>
+             var sussy = new Audio('sus.mp3');
+             function susSound(){
+                sussy.play();
+             }
+            </script>
         </div>
     </body>
 </html>
